d58ad843f95ca9b719c40d2e7b54bdb6b607fb56
Mirkesx <cmarcos46@gmail.com>
2022-09-14 13:26:14 +0200

    feat: layer files to be used by lambdas

---
+++ layers/http-builder.js
@@ -0,0 +1,17 @@
+const buildResponse = (statusCode, body) => {
+    return {
+        statusCode: statusCode,
+        body: JSON.stringify(body),
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type, Origin, X-Requested-With, Accept, Authorization, Access-Control-Allow-Methods, Access-Control-Allow-Headers, Access-Control-Allow-Origin",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET,PUT"
+        },
+    };
+};
+
+
+module.exports = {
+    buildResponse
+};
+

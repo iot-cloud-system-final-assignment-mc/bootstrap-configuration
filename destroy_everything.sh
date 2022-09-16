74c44f227b3412fb092e60f19e6b44a07064f727
Mirkesx <cmarcos46@gmail.com>
2022-09-17 00:25:49 +0200

    fix: revert to aws cloudformation delete-stack (it doens't require an s3) feat: deleting the s3 used to store the sam templates if given

--- destroy_everything.sh
+++ destroy_everything.sh
@@ -3,54 +3,57 @@ if [ -z "$1" ]; then
     exit 0
 fi
 
+AWS_REGION=$1
+SAM_S3_BUCKET=$2
+
 # STACKS WITH RESOURCES
 
 # delete bootstrap
 BOOTSTRAP_S3_BUCKET=$(aws cloudformation list-exports --region ${AWS_REGION} --query "Exports[?Name=='Cloud-Systems-IoT-ApplicationSiteBucket'].Value" --output text)
-aws s3 rm s3://${BOOTSTRAP_S3_BUCKET} --recursive --region $1
-sam delete --stack-name bootstrap-resources --region $1
+aws s3 rm s3://${BOOTSTRAP_S3_BUCKET} --recursive --region $AWS_REGION
+aws cloudformation delete-stack --stack-name bootstrap-resources --region $AWS_REGION
 
 # delete lambda-products
-sam delete --stack-name lambda-products --region $1
+aws cloudformation delete-stack --stack-name lambda-products --region $AWS_REGION
 
 # delete lambda-orders
-sam delete --stack-name lambda-orders --region $1
+aws cloudformation delete-stack --stack-name lambda-orders --region $AWS_REGION
 
 # delete auth-lambda
-sam delete --stack-name auth-lambda --region $1
+aws cloudformation delete-stack --stack-name auth-lambda --region $AWS_REGION
 
 # delete api-gateway
-sam delete --stack-name api-gateway --region $1
+aws cloudformation delete-stack --stack-name api-gateway --region $AWS_REGION
 
 # PIPELINES
 
 # delete web-client-pipeline
 PIPELINE_BUCKET=$(aws cloudformation list-stack-resources --stack-name web-client-pipeline --region ${AWS_REGION} --query "StackResourceSummaries[?LogicalResourceId == 'PipelineBucket'].PhysicalResourceId" --output text)
-aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $1
-sam delete --stack-name web-client-pipeline --region $1
+aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $AWS_REGION
+aws cloudformation delete-stack --stack-name web-client-pipeline --region $AWS_REGION
 
 # delete lambda-products-pipeline
 PIPELINE_BUCKET=$(aws cloudformation list-stack-resources --stack-name lambda-products-pipeline --region ${AWS_REGION} --query "StackResourceSummaries[?LogicalResourceId == 'PipelineBucket'].PhysicalResourceId" --output text)
-aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $1
-sam delete --stack-name lambda-products-pipeline --region $1
+aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $AWS_REGION
+aws cloudformation delete-stack --stack-name lambda-products-pipeline --region $AWS_REGION
 
 # delete lambda-orders-pipeline
 PIPELINE_BUCKET=$(aws cloudformation list-stack-resources --stack-name lambda-orders-pipeline --region ${AWS_REGION} --query "StackResourceSummaries[?LogicalResourceId == 'PipelineBucket'].PhysicalResourceId" --output text)
-aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $1
-sam delete --stack-name lambda-orders-pipeline --region $1
+aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $AWS_REGION
+aws cloudformation delete-stack --stack-name lambda-orders-pipeline --region $AWS_REGION
 
 # delete auth-lambda-pipeline
 PIPELINE_BUCKET=$(aws cloudformation list-stack-resources --stack-name auth-lambda-pipeline --region ${AWS_REGION} --query "StackResourceSummaries[?LogicalResourceId == 'PipelineBucket'].PhysicalResourceId" --output text)
-aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $1
-sam delete --stack-name auth-lambda-pipeline --region $1
+aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $AWS_REGION
+aws cloudformation delete-stack --stack-name auth-lambda-pipeline --region $AWS_REGION
 
 # delete api-gateway-pipeline
 PIPELINE_BUCKET=$(aws cloudformation list-stack-resources --stack-name api-gateway-pipeline --region ${AWS_REGION} --query "StackResourceSummaries[?LogicalResourceId == 'PipelineBucket'].PhysicalResourceId" --output text)
-aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $1
-sam delete --stack-name api-gateway-pipeline --region $1
+aws s3 rm s3://${PIPELINE_BUCKET} --recursive --region $AWS_REGION
+aws cloudformation delete-stack --stack-name api-gateway-pipeline --region $AWS_REGION
 
 # if sam s3 bucket is provided, delete it
-if [ ! -z "$2" ]; then
-    aws s3 rm s3://$2 --recursive --region $1
-    aws s3 rb s3://$2 --region $1
+if [ ! -z "$SAM_S3_BUCKET" ]; then
+    aws s3 rm s3://$SAM_S3_BUCKET --recursive --region $AWS_REGION
+    aws s3 rb s3://$SAM_S3_BUCKET --region $AWS_REGION
 fi
